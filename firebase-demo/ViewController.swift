3e34b901d8fe80850613b3683cfeb272a0554d0e
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2023-01-22 01:49:02 +0900

    Initial Commit

---
+++ firebase-demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firebase-demo
+//
+//  Created by JotaroSugiyama on 2023/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
